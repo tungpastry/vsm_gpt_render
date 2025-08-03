cf21a6063a51d9d39f4aa189ae3f71e514344907
tungpastry <bakerthanhtung@gmail.com>
2025-08-03 15:20:05 +0700

    🔧 Remove default DB_PORT=5432, use env only

--- gpt_query_server.py
+++ gpt_query_server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
-    "port": os.getenv("DB_PORT", 5432),
+    "port": os.getenv("DB_PORT"),
     "dbname": os.getenv("DB_NAME"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD")
